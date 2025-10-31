5ea6069b82f0d9334028a53795acfe90ab6eac2d
Dogukan Dagli <daglidogukan08@gmail.com>
2025-10-31 13:32:00 +0300

    fix : program.cs

---
+++ Server/src/WebAPI/Program.cs
@@ -0,0 +1,45 @@
+﻿using Application;
+using Infrastructure;
+using Microsoft.AspNetCore.RateLimiting;
+using Scalar.AspNetCore;
+using System.Threading.RateLimiting;
+using WebAPI;
+using WebAPI.Modules;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddCors();
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddRateLimiter(options =>
+options.AddFixedWindowLimiter("fixed", opt =>
+{
+    opt.PermitLimit = 100;
+    opt.Window = TimeSpan.FromMinutes(1);
+    opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+    opt.QueueLimit = 100;
+})
+);
+
+builder.Services.AddExceptionHandler<ExceptionHandler>().AddProblemDetails();
+
+var app = builder.Build();
+
+app.MapOpenApi();
+app.MapScalarApiReference();
+
+app.UseCors(policy => policy
+.AllowAnyHeader()
+.AllowCredentials()
+.AllowAnyMethod()
+.SetIsOriginAllowed(t => true));
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseExceptionHandler();
+app.MapControllers().RequireRateLimiting("fixed");
+app.MapAuth();
+
+ExtensionsMiddleware.CreateFirstUser(app);
+app.Run();
