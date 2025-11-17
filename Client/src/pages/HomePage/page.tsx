b7a9273d56c160d945eda7256d451dd02d95093d
Dogukan Dagli <daglidogukan08@gmail.com>
2025-11-17 17:00:55 +0300

    frontend mapbox

--- Client/src/pages/HomePage/page.tsx
+++ Client/src/pages/HomePage/page.tsx
@@ -1,5 +1,37 @@
-import { Box } from "@mui/material";
+import { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+mapboxgl.accessToken = "xxx";
 
 export default function HomePage() {
-  return <Box>HomePage</Box>;
+  // ⭐ Ref'e doğru tip ver
+  const mapContainer = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    // ⭐ İlk render’da null olur → burada durdur
+    if (!mapContainer.current) return;
+
+    const center: [number, number] = [27.124324749601616, 38.384660158052355];
+    const map = new mapboxgl.Map({
+      container: mapContainer.current, // artık HTMLElement olarak doğru tipte
+      style: "mapbox://styles/mapbox/light-v11",
+      center,
+      zoom: 12,
+      attributionControl: false,
+    });
+    new mapboxgl.Marker().setLngLat(center).addTo(map);
+
+    map.addControl(
+      new mapboxgl.AttributionControl({
+        compact: true,
+        customAttribution: "© Mapbox © OpenStreetMap",
+      }),
+      "bottom-right"
+    );
+
+    return () => map.remove();
+  }, []);
+
+  return <div ref={mapContainer} style={{ width: "100%", height: "100vh" }} />;
 }
