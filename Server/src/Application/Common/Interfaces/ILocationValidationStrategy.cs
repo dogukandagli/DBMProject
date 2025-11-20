5ba90765019156645fbe8c7fc7d73459e08613ce
Dogukan Dagli <daglidogukan08@gmail.com>
2025-11-21 00:49:45 +0300

    ValidateLocationStrategy Pattern

---
+++ Server/src/Application/Common/Interfaces/ILocationValidationStrategy.cs
@@ -0,0 +1,8 @@
+﻿using TS.Result;
+
+namespace Application.Common.Interfaces;
+
+public interface ILocationValidationStrategy
+{
+    Task<Result<bool>> ValidateAsync(int userNeighborhoodId, int targetNeighborhoodId);
+}
---
+++ Server/src/Application/Common/Strategies/StrictLocationStrategy.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common.Interfaces;
+using TS.Result;
+
+namespace Application.Common.Strategies;
+
+public class StrictLocationStrategy : ILocationValidationStrategy
+{
+    public Task<Result<bool>> ValidateAsync(int userNeighborhoodId, int targetNeighborhoodId)
+    {
+        if (userNeighborhoodId != targetNeighborhoodId)
+        {
+            return Task.FromResult(Result<bool>.Failure("Sadece kendi mahallenizde işlem yapabilirsiniz."));
+        }
+        return Task.FromResult<Result<bool>>(true);
+    }
+}
