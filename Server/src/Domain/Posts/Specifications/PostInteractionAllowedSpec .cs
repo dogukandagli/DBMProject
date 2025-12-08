a6832db6820959a84826a5343830a9124aeaac2c
Dogukan Dagli <daglidogukan08@gmail.com>
2025-12-09 01:06:44 +0300

    implement command for adding reaction to a post

---
+++ Server/src/Domain/Posts/Specifications/PostInteractionAllowedSpec .cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.Specification;
+using Domain.Posts.Enums;
+
+namespace Domain.Posts.Specifications;
+
+public sealed class PostInteractionAllowedSpec : Specification<Post>
+{
+    public PostInteractionAllowedSpec(int userNeighborhoodId)
+    {
+        Query
+            .Where(p =>
+                    p.PostVisibilty == PostVisibilty.Public
+                    || p.NeighborhoodId == userNeighborhoodId);
+    }
+}
