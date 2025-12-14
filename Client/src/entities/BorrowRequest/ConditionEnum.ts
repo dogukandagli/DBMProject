9675dcc32252e882a623531f16cefeef6c3959df
Dogukan Dagli <daglidogukan08@gmail.com>
2025-12-15 02:20:45 +0300

    offerdialog added

---
+++ Client/src/entities/BorrowRequest/ConditionEnum.ts
@@ -0,0 +1,13 @@
+export const ConditionEnum = {
+  New: 1,
+  LikeNew: 2,
+  Good: 3,
+  Fair: 4,
+  Poor: 5,
+} as const;
+
+export const HandoverMethodEnum = {
+  ComeAndGetIt: 1,
+  IWillBringIt: 2,
+  MeetUp: 3,
+} as const;
