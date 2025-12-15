d0bb2b1e90c1c58247a2195c0260cb1256e9942a
Dogukan Dagli <daglidogukan08@gmail.com>
2025-12-15 12:42:45 +0300

    fix

--- Server/src/Infrastructure/Persistence/Migrations/20251215094136_mg12d.cs
+++ Server/src/Infrastructure/Persistence/Migrations/20251215094136_mg12d.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class mg6s : Migration
+    public partial class mg12d : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
