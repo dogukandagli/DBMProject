dacff62b782b95e7ba124e97a530830c07437206
Dogukan Dagli <daglidogukan08@gmail.com>
2025-12-20 16:48:51 +0300

    Create OfferAcceptedLoanTransactionHandler.cs

---
+++ Server/src/Application/BorrowRequests/EventHandlers/OfferAcceptedLoanTransactionHandler.cs
@@ -0,0 +1,23 @@
+﻿using Domain.BorrowRequests.Events;
+using Domain.LoanTransactions;
+using Domain.LoanTransactions.Repositories;
+using Domain.Shared.ValueObjects;
+using MediatR;
+
+namespace Application.BorrowRequests.EventHandlers;
+
+internal class OfferAcceptedLoanTransactionHandler(
+    ILoanTransactionRepository loanTransactionRepository) : INotificationHandler<OfferAcceptedDomainEvent>
+{
+    public async Task Handle(OfferAcceptedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        TimeSlot timeSlot = TimeSlot.Create(DateTimeOffset.UtcNow.AddDays(1), DateTimeOffset.UtcNow.AddDays(2));
+        LoanTransaction loanTransaction = LoanTransaction.Create(
+            notification.BorrowRequestId,
+            notification.BorrowerId,
+            notification.LenderId,
+            timeSlot
+            );
+        await loanTransactionRepository.AddAsync(loanTransaction);
+    }
+}
