1185d3c34f9dc418bdb1ca2a0ce146187e5e747f
Dogukan Dagli <daglidogukan08@gmail.com>
2025-12-20 16:49:13 +0300

    Create LoanWithQrCodeByHashTokenSpec.cs

---
+++ Server/src/Domain/LoanTransactions/Specifications/LoanWithQrCodeByHashTokenSpec.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Specification;
+
+namespace Domain.LoanTransactions.Specifications;
+
+public class LoanWithQrCodeByHashTokenSpec : SingleResultSpecification<LoanTransaction>
+{
+    public LoanWithQrCodeByHashTokenSpec(string HashToken)
+    {
+        Query
+            .Where(l => l.QrTokens.Any(q => q.TokenHash == HashToken))
+            .Include(l => l.QrTokens.Where(q => q.TokenHash == HashToken));
+    }
+}
