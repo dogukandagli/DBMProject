3941e166f483cf4ccbdad033f6d4232829f7fbd6
Dogukan Dagli <daglidogukan08@gmail.com>
2025-12-22 11:21:11 +0300

    Update MessageCreatedNotificationHandler.cs

--- Server/src/Application/Chat/Messages/EventHandlers/MessageCreatedNotificationHandler.cs
+++ Server/src/Application/Chat/Messages/EventHandlers/MessageCreatedNotificationHandler.cs
@@ -1,7 +1,11 @@
-﻿using Domain.Conversations;
+﻿using Application.Services;
+using Domain.Conversations;
 using Domain.Conversations.Events;
 using Domain.Conversations.Repositorues;
 using Domain.Conversations.Specifications;
+using Domain.Notifications;
+using Domain.Notifications.Enums;
+using Domain.Notifications.Repositories;
 using Domain.Users;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
@@ -12,6 +16,8 @@ namespace Application.Chat.Messages.EventHandlers;
 internal sealed class MessageCreatedNotificationHandler(
     IConversationRepository conversationRepository,
     UserManager<AppUser> userManager,
+    INotificationRepository notificationRepository,
+    INotificationService notificationService,
 ILogger<MessageCreatedNotificationHandler> logger) : INotificationHandler<MessageCreatedEvent>
 {
     public async Task Handle(MessageCreatedEvent notification, CancellationToken cancellationToken)
@@ -25,12 +31,33 @@ ILogger<MessageCreatedNotificationHandler> logger) : INotificationHandler<Messag
         }
 
 
-        AppUser? appUser = await userManager.FindByIdAsync(notification.SenderId.ToString());
+        if (notification.SenderId is null)
+            return;
 
+        AppUser? user = await userManager.FindByIdAsync(notification.SenderId.ToString()!);
+        if (user is null)
+        {
+            logger.LogError($"{notification.SenderId} id li Kullanıcı bulunamadı.");
+            return;
+        }
+
+        string title = "Mesajınız var.";
+        string message = $"{user.FullName} kişisinden mesajınız var.";
 
         foreach (Participant participant in conversation.Participants)
         {
+            if (user.Id != participant.UserId)
+            {
+                Notification notification1 = new Notification(
+                   participant.UserId,
+                   title,
+                   message,
+                   NotificationType.General,
+                   notification.ConversationId);
 
+                await notificationRepository.AddAsync(notification1);
+                await notificationService.SendNotificationToUser(notification1.UserId, notification1);
+            }
         }
     }
 }
