74e2f64966fbc9cdab0482fc316a0e20cb69bb90
Dogukan Dagli <daglidogukan08@gmail.com>
2026-01-16 01:31:02 +0300

    Create CursorPaginatedResponse.cs

---
+++ Server/src/Application/Common/CursorPaginatedResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Common;
+
+public sealed class CursorPaginatedResponse<T>
+{
+    public List<T> Items { get; set; } = new();
+    public DateTimeOffset? NextCursor { get; set; }
+    public bool HasMore { get; set; }
+}
