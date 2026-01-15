f751c1ea06e0adc76f70a9121e2dda336b4bebeb
Dogukan Dagli <daglidogukan08@gmail.com>
2026-01-16 01:30:48 +0300

    Create IMessageReadService.cs

---
+++ Server/src/Application/Chat/Messages/Interfaces/IMessageReadService.cs
@@ -0,0 +1,14 @@
+﻿using Application.Common;
+
+namespace Application.Chat.Messages.Interfaces;
+
+public interface IMessageReadService
+{
+    Task<CursorPaginatedResponse<MessageDto>> GetMessagesAsync(
+        Guid CurrentUserId,
+        Guid ConversationId,
+        DateTimeOffset? OtherUserLastReadAt,
+        DateTimeOffset? Cursor,
+        int PageSize,
+        CancellationToken cancellationToken);
+}
